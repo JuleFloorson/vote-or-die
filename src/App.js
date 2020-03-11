306609b6a7291dfaf6bbf306d20393d9801f325c
JuleFloorson <jubo93@web.de>
2020-03-11 16:37:42 +0100

    Add routing and linked pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
 import Header from "./components/Header";
+import Add from "./components/Add";
+import Result from "./components/Result";
+import Vote from "./components/Vote";
 
 function App() {
   return (
-    <div>
-      <Header />
-    </div>
+    <>
+      <Router>
+        <Header />
+        <main>
+          <div>
+            <Switch>
+              <Route path="/Vote">
+                <Vote />
+              </Route>
+              <Route path="/Result">
+                <Result />
+              </Route>
+              <Route path="/Add">
+                <Add />
+              </Route>
+            </Switch>
+          </div>
+        </main>
+      </Router>
+    </>
   );
 }
 
