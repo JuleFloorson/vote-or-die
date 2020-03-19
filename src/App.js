76261649ae442fa91e5dad729af7807a623fb00b
JuleFloorson <jubo93@web.de>
2020-03-19 14:37:34 +0100

    Add sc to appjs and headerjs

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-import "./App.css";
-import Header from "./components/Header";
+import AppHeader from "./components/AppHeader";
 import AppFooter from "./components/AppFooter";
 import Add from "./components/Add";
 import Result from "./components/Result";
 import Vote from "./components/Vote";
+import styled from "@emotion/styled";
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 function App() {
   return (
     <>
       <Router>
-        <Header />
-        <main className="main">
-          <div>
-            <Switch>
-              <Route path="/vote/:pollId">
-                <Vote />
-              </Route>
-              <Route path="/polls/:pollId">
-                <Result />
-              </Route>
-              <Route path="/">
-                <Add />
-              </Route>
-            </Switch>
-          </div>
-        </main>
+        <AppHeader />
+        <Main>
+          <Switch>
+            <Route path="/vote/:pollId">
+              <Vote />
+            </Route>
+            <Route path="/polls/:pollId">
+              <Result />
+            </Route>
+            <Route path="/">
+              <Add />
+            </Route>
+          </Switch>
+        </Main>
         <AppFooter />
       </Router>
     </>
